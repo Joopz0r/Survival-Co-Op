faef127d2d46f8257efe5ab234bb42bdacbf35d2
diegogmx <diegogmx2000@gmail.com>
2015-07-17 19:05:57 -0300

    now bags of gold make sound when picked
    commit asap

--- game/dota_addons/horde_git/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/horde_git/scripts/vscripts/addon_game_mode.lua
@@ -21,6 +21,7 @@ function Precache( context )
 	--PrecacheResource( "particle", "particles/generic_gameplay/winter_effects_hero.vpcf", context )
 	PrecacheResource( "particle", "particles/items2_fx/veil_of_discord.vpcf", context )	
 	PrecacheResource( "particle_folder", "particles/frostivus_gameplay", context )
+
 	PrecacheItemByNameSync( "item_tombstone", context )
 	PrecacheItemByNameSync( "item_bag_of_gold", context )
 	PrecacheItemByNameSync( "item_bag_of_gold_survival", context )
--- game/dota_addons/horde_git/scripts/vscripts/bag_of_gold_survival.lua
+++ game/dota_addons/horde_git/scripts/vscripts/bag_of_gold_survival.lua
@@ -1,6 +1,9 @@
 function BagOfGoldPicked( event )
 	item = event.ability
-
+	
+	PrecacheResource( "soundfile", "soundevents/game_sounds_custom.vsndevts", context)
+	item:EmitSound("GoldBagDrop")
+	
 	for i = 0, DOTA_MAX_TEAM_PLAYERS-1 do
 		if PlayerResource:IsValidPlayer(i) then
 			PlayerResource:ModifyGold(i, item:GetCurrentCharges(), true, DOTA_ModifyGold_Unspecified )
