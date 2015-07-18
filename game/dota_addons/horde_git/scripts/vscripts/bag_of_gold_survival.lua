90dba101384e1fd122546a8d5d8ee4bcde5ae1b4
diegogmx <diegogmx2000@gmail.com>
2015-07-18 14:31:56 -0300

    double cast workaround

--- game/dota_addons/horde_git/scripts/vscripts/bag_of_gold_survival.lua
+++ game/dota_addons/horde_git/scripts/vscripts/bag_of_gold_survival.lua
@@ -4,7 +4,7 @@ function BagOfGoldPicked( event )
 	
 	for i = 0, DOTA_MAX_TEAM_PLAYERS-1 do
 		if PlayerResource:IsValidPlayer(i) then
-			PlayerResource:ModifyGold(i, item:GetCurrentCharges(), true, DOTA_ModifyGold_Unspecified )
+			PlayerResource:ModifyGold(i, item:GetCurrentCharges()/2, true, DOTA_ModifyGold_Unspecified )
 		end
 	end
 end
